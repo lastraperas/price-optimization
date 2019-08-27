a53dfdc085497d133e920b59602f1ecb629be3ee
merunga <merunga@gmail.com>
2019-08-27 14:45:10 -0500

    lint

--- lamdba/textToNumber.js
+++ lamdba/textToNumber.js
@@ -2,6 +2,6 @@ const allMarcas = require('./data/marcas.json')
 const allTipos = require('./data/tipos.json')
 
 module.exports = {
-  convertTipo = tipo => allTipos.indexOf(tipo),
-  convertMarca = marca => allMarcas.indexOf(marca),
+  convertTipo: tipo => allTipos.indexOf(tipo),
+  convertMarca: marca => allMarcas.indexOf(marca),
 };
